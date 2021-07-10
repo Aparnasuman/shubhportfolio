b3c89450a00db2b1ac4ecb69d4502581250ffc26
Aparnasuman <aprana@doodleblue.com>
2021-07-10 18:27:05 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import Home from '../components/home-page/home';
 import { projectsList } from "./../data/projects-list";
 import Meta from '../components/layout/meta';
-
+//@ts-ignore
 export default function Index({ projects }) {
   return (
     <Fragment>
